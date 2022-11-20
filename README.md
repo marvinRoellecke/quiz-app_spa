6492bf9b1a0af8e01094e27822b11fa1c397506e
Marvin Roellecke <marvin@roellecke.com>
2022-11-20 20:30:01 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Quiz App as Single Page Application
---
+++ index.js
@@ -0,0 +1,26 @@
+const questions = [
+  {
+    id: 0,
+    question: "Das ist Frage eins",
+    answer: "Antwort eins",
+    categories: ["eins", "zwei", "drei"],
+    bookmarked: false,
+  },
+  {
+    id: 2,
+    question: "Das ist Frage zwei",
+    answer: "Anwort Zwei",
+    categories: ["eins", "zwei", "drei"],
+    bookmarked: true,
+  },
+
+  {
+    id: 4,
+    question:
+      "Dies ist eine sehr lange Frage, um zu testen, ob und wie die Darstellung einer solchen letztlich dem Gesamtstyle schadet.",
+    answer:
+      "Die Antwort wiederum ist im Vergleich zu Frage recht kurz geraten. Belassen wir es doch dabei!",
+    categories: ["eins", "zwei", "drei"],
+    bookmarked: false,
+  },
+];
